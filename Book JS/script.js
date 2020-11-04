db64dae1662c9a33f244b6da2a659a1fcc747aff
unknown <madalenafvilar@gmail.com>
2020-11-04 12:15:08 +0100

    playing

---
+++ Book JS/script.js
@@ -0,0 +1,15 @@
+let questions = ["What is my name?", "How old am I?", "Where was I born?"]
+let elQuestions = document.getElementById("question")
+elQuestions.innerHTML = questions[2]
+
+let optionA = ["Carolina", "16", "Jerusalem"]
+let elOptionA = document.getElementById("a")
+elOptionA.innerHTML = optionA[2]
+
+let optionB = ["Marta", "25", "Lisbon"]
+let elOptionB = document.getElementById("b")
+elOptionB.innerHTML = optionB[2]
+
+let optionC = ["Madalena", "32", "Stockholm"]
+let elOptionC = document.getElementById("c")
+elOptionC.innerHTML = optionC[2]
